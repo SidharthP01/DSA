e3f977058999a2cd9c69d6c203d1e073d54cdd25
SidharthP01 <105432075+SidharthP01@users.noreply.github.com>
2024-07-31 14:12:09 +0530

    4

---
+++ gcd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Enter the number : ";
+    cin>>n1>>n2;
+    int gcd=1;
+    for(int i=1;i<=min(n1,n2);i++)
+    {
+        if(n1%i==0 && n2%i==0)
+            gcd=i;
+    }
+    cout<<gcd<<endl;
+    return 0;
+}
